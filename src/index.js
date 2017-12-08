bfe894651578ec9b565cdac0eadd0812bf1454be
Jiang Haiyun <jiang.haiyun@gmail.com>
2017-12-08 15:49:38 +0800

    Tree Shaking

--- src/index.js
+++ src/index.js
@@ -1,39 +1,14 @@
-import _ from 'lodash';
-import './style.css'
-import Icon from './icon.png';
-import Data from './data.xml';
-import printMe from './print.js';
+import { cube } from './math.js';
 
 function component() {
-  var element = document.createElement('div');
-  var btn = document.createElement('button');
+  var element = document.createElement('pre');
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
-
-  // Add the image to our existing div.
-  var myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-
-  console.log(Data);
-
+  element.innerHTML = [
+      'Hello webpack!',
+      '5 cubed = ' + cube(5)
+  ].join('\n\n');
 
   return element;
 }
 
 document.body.appendChild(component());
-
-if (module.hot) {
-    module.hot.accept('./print.js', function(){
-        console.log("Accepting the updated printMe module!");
-        printMe();
-    })
-}
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const webpack = require("webpack");
 
 module.exports = {
@@ -22,7 +23,8 @@ module.exports = {
           title: 'Output Management by HtmlWebpackPlugin'
       }),
       new webpack.NamedModulesPlugin(),
-      new webpack.HotModuleReplacementPlugin()
+      new webpack.HotModuleReplacementPlugin(),
+      new UglifyJSPlugin()
   ],
   module: {
       rules: [
