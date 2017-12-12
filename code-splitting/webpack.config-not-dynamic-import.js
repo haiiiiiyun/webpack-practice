6a9d0fe3d2ab69ac1912b72df53f7d405cf02aac
Jiang Haiyun <jiang.haiyun@gmail.com>
2017-12-12 15:00:18 +0800

    add code-splitting demo

---
+++ code-splitting/webpack.config-not-dynamic-import.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    index: './src/index.js',
+    another: './src/another-module.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Code Splitting'
+    }),
+    new CleanWebpackPlugin(['dist']),
+    new webpack.optimize.CommonsChunkPlugin({
+        name: 'common' // Specify the common bundle's name.
+    })
+  ]
+};
---
+++ code-splitting/webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index: './src/index.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Code Splitting'
+    }),
+    new CleanWebpackPlugin(['dist'])
+  ]
+};
