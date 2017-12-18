47c5734f0f4d11df6e2d226c5413b135ef2e3588
Jiang Haiyun <jiang.haiyun@gmail.com>
2017-12-18 11:30:06 +0800

    author-libraries

---
+++ authoring_library/webpack.config.js
@@ -0,0 +1,19 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'webpack-numbers.js',
+    library: 'webpackNumbers',
+    library: 'umd'
+  },
+  externals: {
+    lodash: {
+      commonjs: 'lodash',
+      commonjs2: 'lodash',
+      amd: 'lodash',
+      root: '_'
+    }
+  }
+};
